ee8694e7d04017c5157a5b15a8a7ca807d22dee3
P Sai Charan <90904044+saicharan1901@users.noreply.github.com>
2024-10-30 21:12:50 +0530

    fixed responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
 "use client";
 
 import Navbar from "@/components/navbar";
+import Image from "next/image";
+import { cn } from "@/utils/cn";
+import { TypewriterEffectSmooth } from "@/components/typewriter";
 import { useRef } from "react";
 import Timeline from "./sections/timeline";
 import Projects from "./sections/project";
 import Introduction from "./sections/introduction";
-import Footer from "./sections/footer";
 
 export default function Home() {
+  const words = [
+    { text: "Hi, I am", className: "text-neutral-500 text-3xl" },
+    { text: "Sai Charan :)", className: "text-[#CFFF04] text-3xl" },
+  ];
+
   const scrollToSection = (ref: React.RefObject<HTMLElement>) => {
     ref.current?.scrollIntoView({ behavior: "smooth" });
   };
@@ -19,26 +26,38 @@ export default function Home() {
 
   return (
     <div className="bg-black text-white min-h-screen md:mx-auto md:max-w-[50%] max-w-[100%]">
-      <Navbar
-        onLinkClick={(section: string) => {
-          if (section === "Introduction") scrollToSection(introRef);
-          else if (section === "Education") scrollToSection(educationRef);
-          else if (section === "Projects") scrollToSection(projectsRef);
-          else if (section === "Experience") scrollToSection(experienceRef);
-        }}
-      />
+      <div className="flex flex-col">
+        <Navbar
+          onLinkClick={(section: string) => {
+            if (section === "Introduction") scrollToSection(introRef);
+            else if (section === "Education") scrollToSection(educationRef);
+            else if (section === "Projects") scrollToSection(projectsRef);
+            else if (section === "Experience") scrollToSection(experienceRef);
+          }}
+        />
 
-      <Introduction ref={introRef} />
+        {/* Apply padding only on small screens */}
+        <section
+          ref={introRef}
+          className="lg:pt-20 xl:pt-0 pt-20  flex flex-col items-center justify-center text-center"
+        >
+          <Introduction />
+        </section>
+      </div>
 
-      <section ref={educationRef} className="flex items-center justify-center text-center">
+      <section
+        ref={educationRef}
+        className="sm:pt-20 md:pt-0 flex items-center justify-center text-center"
+      >
         <Timeline />
       </section>
 
-      <section ref={projectsRef} className="flex items-center mb-10 justify-center text-center">
+      <section
+        ref={projectsRef}
+        className="sm:pt-20 md:pt-0 flex items-center justify-center text-center"
+      >
         <Projects />
       </section>
-
-      {/* <Footer /> */}
     </div>
   );
 }
